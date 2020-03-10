698d13b2a32f19343515a484bd5e1d7915706edf
Davi Henrique Silva de Oliveira <davoliveira@gertec.local>
2020-03-10 12:02:00 -0300

    Criação de Testes do ContaTeste

--- src/test/java/exercicios/ContaTeste.java
+++ src/test/java/exercicios/ContaTeste.java
@@ -1,5 +1,72 @@
 package exercicios;
 
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ContaTeste {
+    private Conta c1;
+
+    @BeforeEach
+    public void beforeEach(){
+        c1 = new Conta();
+        c1.numero = 1;
+        c1.saldo = 500.00;
+        c1.limite = 1000.00;
+    }
+    @AfterEach
+    public void afterEach(){
+        c1.numero = null;
+    }
+    @Test
+    public void criarObjetoConta(){
+        assertNotNull(c1);
+    }
+    @Test
+    public void presencaAtributoConta(){
+        assertNotNull(c1.numero);
+        assertNotNull(c1.saldo);
+        assertNotNull(c1.limite);
+    }
+    @Test
+    public void metodoDepositar(){
+        Double valorDepositado = 200.00;
+        Double valorEsperado = c1.depositar();   // verificar erro
+        assertEquals(valorDepositado,valorEsperado);
+    }
+    @Test
+    public void metodoSacar(){
+    }
+    @Test
+    public void metodoSacarSaldoSuficiente() {
+    }
+    @Test
+    public void metodoSacarSaldoInsuficiente() {
+    }
+    //-----------------------------
+
+    @Test
+    public void metodoTransferir(){
+    }
+    @Test
+    public void metodoTransferirValorValido() {
+    }
+    @Test
+    public void metodoTransferirValorInvalido() {
+    }
+    //------------------------------------------
+    @Test
+    public void metodoDepositar(){
+    }
+
+    @Test
+    public void metodoDepositarValorInvalido() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> c1.depositar(-200.0));
+        assertEquals("Valor deve ser maior que zero.", exception.getMessage());
+    }
+
 
 }
